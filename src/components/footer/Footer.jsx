823ae60b07228a5cb0af1bb1486fd462765a8562
oussama ouakassi <lmoutchoo@gmail.com>
2025-09-19 01:08:09 +0100

    Refactor Footer component to use SocialIcon for enhanced social link display; clean up unused styles in Footer.css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,13 +4,16 @@ import SocialLink from "./../SocialLink";
 import { Link } from "react-router-dom";
 
 import { socialData } from "../../data/socialData";
+import SocialIcon from "../contact/SocialIcon";
 
 const Footer = () => (
   <footer>
     <div className="footer__container container">
       <div className="footer__links">
-        {socialData.map(({ link, icon }) => {
-          return <SocialLink key={link} link={link} icon={icon} />;
+        {socialData.map(({ link, icon, title, color }) => {
+          return (
+            <SocialIcon link={link} icon={icon} title={title} color={color} />
+          );
         })}
       </div>
       <p className="footer__copyright">
